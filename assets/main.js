1d58b9824ea5b86ee41ce39e4fdf20cb21e76023
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-17 17:37:47 +0200

    add for loop for generate 10 random mails

--- assets/main.js
+++ assets/main.js
@@ -6,21 +6,17 @@
 
 
 const UlEl = document.querySelector('ul')
-let emailArray = []
 for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(response => {
             let randomEmail = response.data.response
-            emailArray.push(randomEmail)
             let li = document.createElement('li')
             li.append(randomEmail)
             UlEl.appendChild(li)
             }
-           
         )    
 }
 
-console.log(emailArray)
 
 
             
