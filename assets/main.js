7f8288d3639cf90223b6dab67a320ed786a28e84
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-17 17:16:24 +0200

    add for loop for generate 10 random mails

--- assets/main.js
+++ assets/main.js
@@ -4,30 +4,28 @@
 //Abbellire con CSS o Bootstrap
 //Inserire un bottone che al click fetchi altre 10 mail (sostituendo le altre)
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response =>  {
-    const UlEl = document.querySelector('ul')
-    console.log(UlEl); 
-    /* console.log(response.data); 
-    const randomMail = response
-    console.log(randomMail);  */
-    const randomEmail = response.data.response
-    console.log(randomEmail);
-    const randomEmailEl = document.createElement('li')
-    UlEl.appendChild(randomEmail)
 
-})
-
-
-
-function generateRandomEmails(email){
-    for (let i = 0; i <= 10; i++) {
-        const mail = email[i];
-        mail = mail*10
-    }
-    console.log(mail);
+const UlEl = document.querySelector('ul')
+let emailArray = []
+for (let i = 0; i < 10; i++) {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => {
+            let randomEmail = response.data.response
+            emailArray.push(randomEmail)
+            }
+        )
+        
+}
+for (let i = 0; i < emailArray.length; i++) {
+    const mail = emailArray[i];
     
-    return mail
 }
 
-generateRandomEmails(randomEmail)
+console.log(emailArray)
+/* ;
+let li = document.createElement('li')
+li.append(emailArray)
+UlEl.appendChild(li) */
+
+            
+            
